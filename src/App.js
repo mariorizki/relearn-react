5dc3acd6924b57646015e02baab66a07c7b941a9
Mario Rizki <mariorizki07@gmail.com>
2019-05-20 04:47:11 +0700

    implement rendering persons conditionally

--- src/App.js
+++ src/App.js
@@ -4,24 +4,35 @@ import Person from './components/Person';
 
 class App extends Component {
   state = {
-    persons: [{ name: 'Max', age: 29 }, { name: 'Manu', age: 31 }]
+    persons: [{ name: 'Max', age: 29 }, { name: 'Manu', age: 31 }],
+    showPersons: false
   };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, Im a React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[0].age}
-        >
-          My Hobbies: Racing
-        </Person>
+        <button onClick={this.togglePersonHandler}>Show Persons</button>
+        {this.state.showPersons ? (
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+            />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[0].age}
+            >
+              My Hobbies: Racing
+            </Person>
+          </div>
+        ) : null}
       </div>
     );
   }
