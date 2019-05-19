e923d1e487b177b773f557897a6bb19603c0e6ea
Mario Rizki <mariorizki07@gmail.com>
2019-05-20 02:55:42 +0700

    add state to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './components/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, Im a React App</h1>
-      <p>This is really working!</p>
-      <Person name="Max" age="29" />
-      <Person name="Manu" age="31">
-        My Hobbies: Racing
-      </Person>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [{ name: 'Max', age: 29 }, { name: 'Manu', age: 31 }]
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, Im a React App</h1>
+        <p>This is really working!</p>
+        <button>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[0].age}
+        >
+          My Hobbies: Racing
+        </Person>
+      </div>
+    );
+  }
 }
-
 export default App;
